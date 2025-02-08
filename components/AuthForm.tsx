0fb1df2814423f5419b8b808ad6120a8c77848a4
Legion204 <ri46801@gmail.com>
2025-02-08 21:17:58 +0600

    added auth pages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import {
+  DefaultValues,
+  FieldValues,
+  SubmitHandler,
+  useForm,
+  UseFormReturn,
+} from "react-hook-form";
+import { z, ZodType } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+interface Props<T extends FieldValues> {
+  schema: ZodType<T>;
+  defaultValues: T;
+  onSubmit: (data: T) => Promise<{ success: boolean; error?: string }>;
+  type: "SIGN_IN" | "SIGN_UP";
+}
+
+const AuthForm = <T extends FieldValues>({
+  type,
+  schema,
+  defaultValues,
+  onSubmit,
+}: Props<T>) => {
+
+    const isSignIn = type === 'SIGN_IN'
+
+  const form: UseFormReturn<T> = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValues as DefaultValues<T>,
+  });
+
+  const handleSubmit: SubmitHandler<T> = async (data) => {};
+
+  return (
+    <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold text-white">{isSignIn ? "Welcome back to Bibliora":"Create your library account"}</h1>
+        <p className="text-light-100">
+            {isSignIn? "Please sign in with email password.":"Please use your university Id and university Card to sign up for library account."}
+        </p>
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default AuthForm;
