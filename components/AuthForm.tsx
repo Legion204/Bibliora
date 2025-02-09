4ec19df2689127105760d16d8009c6f7f4a38881
Legion204 <ri46801@gmail.com>
2025-02-09 18:59:47 +0600

    added auth form

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -4,11 +4,12 @@ import React from "react";
 import {
   DefaultValues,
   FieldValues,
+  Path,
   SubmitHandler,
   useForm,
   UseFormReturn,
 } from "react-hook-form";
-import { z, ZodType } from "zod";
+import { ZodType } from "zod";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -21,6 +22,9 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import { FIELD_NAMES, FIELD_TYPES } from "@/constants";
+import ImageUpload from "./ImageUpload";
 
 interface Props<T extends FieldValues> {
   schema: ZodType<T>;
@@ -35,8 +39,7 @@ const AuthForm = <T extends FieldValues>({
   defaultValues,
   onSubmit,
 }: Props<T>) => {
-
-    const isSignIn = type === 'SIGN_IN'
+  const isSignIn = type === "SIGN_IN";
 
   const form: UseFormReturn<T> = useForm({
     resolver: zodResolver(schema),
@@ -47,31 +50,54 @@ const AuthForm = <T extends FieldValues>({
 
   return (
     <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-semibold text-white">{isSignIn ? "Welcome back to Bibliora":"Create your library account"}</h1>
-        <p className="text-light-100">
-            {isSignIn? "Please sign in with email password.":"Please use your university Id and university Card to sign up for library account."}
-        </p>
-        <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
+      <h1 className="text-2xl font-semibold text-white">
+        {isSignIn ? "Welcome back to Bibliora" : "Create your library account"}
+      </h1>
+      <p className="text-light-100">
+        {isSignIn
+          ? "Please sign in with email password."
+          : "Please use your university Id to sign up for library account."}
+      </p>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-6 w-full"
+        >
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    {field.name === "universityCard" ? (
+                      <ImageUpload></ImageUpload>
+                    ) : (
+                      <Input required type={FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]} {...field} 
+                      className="form-input"/>
+                    )}
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button type="submit" className="form-btn">{isSignIn? "Sign In":"Sign Up"}</Button>
         </form>
       </Form>
+      <p className="text-center text-base font-medium">
+        {isSignIn ? "New to Bibliora?" : "Already have an account?"}
+        <Link
+          href={isSignIn ? "/sign-up" : "sign-in"}
+          className="font-bold text-primary"
+        >
+          {isSignIn ? "Create an account" : "Sign In"}
+        </Link>
+      </p>
     </div>
   );
 };
